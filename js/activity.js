7df9dc7e4d860fdac3b34056c676aba2741a0391
Anlibraly <htl19921204@hotmail.com>
2016-03-03 11:35:49 +0800

    修改了列表以及其与leancloud的通信
    把leancloud通信跟上以及列表美化

--- js/activity.js
+++ js/activity.js
@@ -12,21 +12,45 @@
         $scope.getMore = function(){
 
             if($scope.more){
-                var param = {"content":$scope.cmt_content,"userid":"网友","loc":loc,"actid":$scope.actid};
-                AV.Cloud.run('addComment', param).then(function(res) {
-                    var a = [];
-                    a.push(res);
-                    $scope.comments = a.concat($scope.comments);
-                    console.log($scope.comments);
-                    $scope.cmt_content = "";
-                    showModel('评论成功');
-                    $scope.$apply();
+                var param = {"page":$scope.page,"city":-1,"prov":-1};
+                AV.Cloud.run('queryActivity', param).then(function(res) {
+                if(res.counts){
+                    var events = res.acts; 
+                    var tmp_eve = new Array();
+                    for (i in events) {
+                        var e = events[i];
+                        if (e.fst == 1) {
+                            if (tmp_eve!=null&&tmp_eve.length>0) {
+                                var a = tmp_eve[0];
+                                for (var i=0; i<$scope.activities.length; i++) {
+                                    if($scope.activities[i][0].m_day==a.m_day){
+                                        $scope.activities[i].push(tmp_eve);
+                                    }
+                                }
+                                if (i==$scope.activities.length) {
+                                    $scope.activities.push(tmp_eve);
+                                };
+                            };
+                            tmp_eve = new Array();
+                        }
+                        tmp_eve.push(e);
+                    }
+                    if (tmp_eve!=null&&tmp_eve.length>0) {
+                         $scope.activities.push(tmp_eve);
+                    }
+                    $scope.more = true;
+                    $scope.$apply();    
+                }else{
+                         showModel('没有更多活动了');
+                        $scope.more = false;                   
+                }
+
                 },function(error) {
                     showModel('系统错误');
                     console.log(error);
                 });
-                
-                $.ajax({
+
+/*                $.ajax({
                     type: 'POST',
                     url: 'http://192.168.0.103:8080/autismsys/sendHttp.action',
                     data: 'func=functions/queryActivity&param=page='+$scope.page+'_city=-1_prov=-1',
@@ -64,7 +88,7 @@
                             $scope.more = false;
                         } 
                     }
-                });
+                });*/
             }
         }
         
